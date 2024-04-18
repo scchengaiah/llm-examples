349313fbf0965e971257695bfd4aa2b99c6ca544
S C Chengaiah <scchengaiah24892@gmail.com>
2024-04-18 21:32:29 +0200

    Latest commit.

--- 01_guardrails/00_nemo_guardrails_exploration/02_langchain_related_examples/test_guardrails_standalone.py
+++ 01_guardrails/00_nemo_guardrails_exploration/02_langchain_related_examples/test_guardrails_standalone.py
@@ -28,6 +28,53 @@ bot_streaming_handler = ContextVar("bot_streaming_handler", default=None)
 model_id="anthropic.claude-3-haiku-20240307-v1:0"
 #model_id="anthropic.claude-instant-v1"
 
+###### Prompt Template
+CONDENSE_QUESTION_PROMPT_STR = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in English language. Avoid presenting empty standalone questions. If ambiguity arises, retain the follow up question as is. Do not include any other content other than the rephrased question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = ChatPromptTemplate.from_template(CONDENSE_QUESTION_PROMPT_STR)
+
+QA_PROMPT_STR = """You are a friendly chatbot assistant that responds in a conversational manner to users' question on company's policies. 
+Respond in 1-2 complete sentences, unless specifically asked by the user to elaborate on something. Use "Context" to inform your answers.
+Do not make up answers if the question is out of "Context". Do not respond with any general information or advice that is not related to the context.
+If the "Question" is not clear, then refer to the "Chat History" and respond appropriately.
+If the "Question" is a greeting or a compliment, then respond appropriately without considering the context.
+
+---
+Chat History:
+{chat_history}
+---
+Context:
+{context}
+---
+Question:
+{question}
+---
+Response:
+"""
+QA_PROMPT = ChatPromptTemplate.from_template(QA_PROMPT_STR)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+#### Initialize Azure Search and Embeddings
+azure_search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
+azure_search_api_key = os.getenv("AZURE_SEARCH_API_KEY")
+azure_search_index = os.getenv("AZURE_SEARCH_INDEX")
+
+azure_embedding_deployment = os.getenv("AZURE_EMBEDDING_MODEL_DEPLOYMENT_NAME")
+
+embeddings = BedrockEmbeddings(region_name=os.getenv("AWS_REGION"), model_id=os.getenv("AWS_LLM_EMBEDDINGS_ID"))
+
+vector_store = AzureSearch(
+    azure_search_endpoint=azure_search_endpoint,
+    azure_search_key=azure_search_api_key,
+    index_name=azure_search_index,
+    embedding_function=embeddings.embed_query,
+)
+
 async def execute_with_guardrails():
 
     ###### Guardrail Content
@@ -56,35 +103,6 @@ async def execute_with_guardrails():
             engine: amazon_bedrock        
     """
 
-    ###### Prompt Template
-    CONDENSE_QUESTION_PROMPT_STR = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in English language. Avoid presenting empty standalone questions. If ambiguity arises, retain the follow up question as is. Do not include any other content other than the rephrased question.
-
-    Chat History:
-    {chat_history}
-    Follow Up Input: {question}
-    Standalone question:"""
-    CONDENSE_QUESTION_PROMPT = ChatPromptTemplate.from_template(CONDENSE_QUESTION_PROMPT_STR)
-
-    QA_PROMPT_STR = """You are a friendly chatbot assistant that responds in a conversational manner to users' question on company's policies. 
-    Respond in 1-2 complete sentences, unless specifically asked by the user to elaborate on something. Use "Context" to inform your answers.
-    Do not make up answers if the question is out of "Context". Do not respond with any general information or advice that is not related to the context.
-    Respond to greetings or compliments in a positive manner and let the user know your capability.
-
-    ---
-    Context:
-    {context}
-    ---
-    Question:
-    {question}
-    ---
-    Response:
-    """
-    QA_PROMPT = ChatPromptTemplate.from_template(QA_PROMPT_STR)
-
-    DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
-
-
-
     llm_for_chain = BedrockChat(
                 region_name=os.getenv("AWS_REGION"),
                 model_id = model_id,
@@ -97,21 +115,7 @@ async def execute_with_guardrails():
         streaming=True
     )
 
-    #### Initialize Azure Search and Embeddings
-    azure_search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
-    azure_search_api_key = os.getenv("AZURE_SEARCH_API_KEY")
-    azure_search_index = os.getenv("AZURE_SEARCH_INDEX")
 
-    azure_embedding_deployment = os.getenv("AZURE_EMBEDDING_MODEL_DEPLOYMENT_NAME")
-
-    embeddings = BedrockEmbeddings(region_name=os.getenv("AWS_REGION"), model_id=os.getenv("AWS_LLM_EMBEDDINGS_ID"))
-
-    vector_store = AzureSearch(
-        azure_search_endpoint=azure_search_endpoint,
-        azure_search_key=azure_search_api_key,
-        index_name=azure_search_index,
-        embedding_function=embeddings.embed_query,
-    )
 
     #### Initialize Chain
     def combine_documents(docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"):
@@ -234,12 +238,54 @@ async def execute_chain():
                 model_id = model_id,
                 streaming=True
             )
-    chain = prompt | model
-    result = chain.invoke({"question": "What is the meaning of life in 300 words?"},
-                         config=runnable_config)
+
+    #### Initialize Vectorstore
+
+    #### Initialize Chain
+    def combine_documents(docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"):
+        doc_strings = [format_document(doc, document_prompt) for doc in docs]
+        return document_separator.join(doc_strings)
+
+    # Takes the standalone question as the input and the context as the vectorstore.
+    # Confine our retrieval to Germany policies loaded.
+    search_kwargs = {"filters": "location eq 'Germany'", "k": 3}
+    context = {
+        "context": itemgetter("question") | vector_store.as_retriever(search_kwargs=search_kwargs) | combine_documents,
+        "question": lambda x: x["question"],
+        "chat_history": lambda x: x["chat_history"]
+    }
+
+    inputs = RunnableParallel(
+        question=RunnablePassthrough.assign(
+            chat_history=lambda x: get_buffer_string(x["chat_history"])
+        )
+                 | CONDENSE_QUESTION_PROMPT
+                 | model
+                 | StrOutputParser(),
+    )
+
+    chain_with_follow_up_question = inputs | context | QA_PROMPT | model
+    chain_without_follow_up_question = context | QA_PROMPT | model
+
+    query = "Hello"
+    chat_history = [
+        HumanMessage(content="Explain our company's leave policy ?"),
+        AIMessage(
+            content="Employees are eligible for 30 days of regular leaves for a given calendar year (1st Jan till 31st Dec) and must apply for planned leaves with prior approval from their project manager and designated reporting manager. In case of emergency, employees must inform their immediate superior and HR, and all leaves must be applied through the Intelizign Intranet Portal."),
+        HumanMessage(content="Explain our company loan policy"),
+        AIMessage(
+            content="Our loan policy allows relocated employees in Germany to request a loan for a flat deposit up to 3,000€. To request a loan, employees must email the HR department with the purpose and required amount, and sign a document prepared by HR before repayment within one financial year.")
+    ]
+
+    #chat_history = []
+
+    print(f"Executing query: {query}")
+
+    result = chain_without_follow_up_question.invoke({"question": query,
+                                                      "chat_history": chat_history})
     print("\n")
     print("RESULT:")
-    print(result)
+    print(result.content)
 
 if __name__ == "__main__":
-    asyncio.run(execute_with_guardrails())
+    asyncio.run(execute_chain())
